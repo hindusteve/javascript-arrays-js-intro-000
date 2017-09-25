815ed2a90befe996760b36a8cd440c7632139647
Abhijit <abhijit.khanna@live.com>
2017-09-25 18:41:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,17 +10,21 @@ function destructivelyAddElementToBeginningOfArray (array, element) {
 
   array.unshift(element);
 
+  return array;
+
 }
 
 function addElementToEndOfArray(array, element) {
 
-  return ([...array, element])
+  return ([...array, element]);
 
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
 
-  array.push(element)
+  array.push(element);
+
+  return array;
 
 }
 
